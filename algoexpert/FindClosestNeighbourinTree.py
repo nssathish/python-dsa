e930096b1dc218664edbaa099057dbe6d573c40d
Sathish Kumar <neosathishgct@gmail.com>
2020-12-25 19:40:29 +0530

    Finding closest Neighbour in Tree - wrong answering algorithm

--- algoexpert/FindClosestNeighbourinTree.py
+++ algoexpert/FindClosestNeighbourinTree.py
@@ -1,96 +1,66 @@
-'''
-        10
-      /    \
-     5      15
-   /   \   /  \
-  2     5 13  22
- /          \
-1            14
+def FindClosestNeighbourinBST(tree, target):
+	closest = int(tree["root"])
+	treeNodes = tree["nodes"]
+	nodes = {}
 
-input dictionary from algoexpert
-o
-{
-  "nodes": [
-    {"id": "10", "left": "5", "right": "15", "value": 10},
-    {"id": "15", "left": "13", "right": "22", "value": 15},
-    {"id": "22", "left": null, "right": null, "value": 22},
-    {"id": "13", "left": null, "right": "14", "value": 13},
-    {"id": "14", "left": null, "right": null, "value": 14},
-    {"id": "5", "left": "2", "right": "5-2", "value": 5},
-    {"id": "5-2", "left": null, "right": null, "value": 5},
-    {"id": "2", "left": "1", "right": null, "value": 2},
-    {"id": "1", "left": null, "right": null, "value": 1}
-  ],
-  "root": "10"
-}
-target = 12
-'''
+	for node in treeNodes:
+		nodes[node["id"]] = node
 
-def FineClosestNeighbourinBST(tree, target):
-    closest = int(tree["root"])
-    nodes = tree["nodes"]
-    leftmin = None
-    leftMinNode = None
-    leftNode = node["left"]
+	rootmin = None
+	if closest:
+		rootmin = target - closest
 
-    rightmin = None
-    rightMinNode = None
-    rightNode = node["right"]
+	leftmin = None
+	leftMinNode = None
+	leftNode = nodes[nodes[tree["root"]]["left"]]
 
-    while leftNode:
-        if leftmin is None or abs(target - int(node["left"])) < leftmin:
-            leftmin = abs(target - int(node["left"])) 
-            leftMinNode = node["value"]
+	rightmin = None
+	rightMinNode = None
+	rightNode = nodes[nodes[tree["root"]]["right"]]
 
-        leftNode = node[node["left"]]
+	while leftNode:
+		if leftmin is None or abs(target - int(leftNode["value"])) < leftmin:
+			leftmin = abs(target - int(leftNode["value"])) 
+			leftMinNode = leftNode["value"]
 
-    while rightNode:
-        if rightmin is None or abs(target - int(node["right"])) < rightmin)   
-            rightmin = abs(target - int(node["right"]))
-            rightMinNode = node["value"]
+		if leftNode["left"]:
+			leftNode = nodes[leftNode["left"]]
+		else:
+			break
 
-        rightNode = node[node["right"]]
+	while rightNode:
+		if rightmin is None or abs(target - int(rightNode["value"])) < rightmin:
+			rightmin = abs(target - int(rightNode["value"]))
+			rightMinNode = rightNode["value"]
+
+		if rightNode["right"]:
+			rightNode = nodes[rightNode["right"]]
+		else:
+			break
 
 
-    if leftmin < rightmin:
-        return leftNode
-    else:
-        return rightNode
-
-
-class Node:
-    def __init__(self, data=None):
-        self.data = data
-        self.left = None
-        self.right = None
-
-
-    def insert(self, value):
-        if self.data:
-            if value < self.data:
-                if self.left:
-                    self.left.insert(value)
-                else:
-                    self.left = Node(value)
-            elif self.right:
-                self.right.insert(value)
-            else:
-                self.right = Node(value)
-        else:
-            self.data = value
-
-
-    def printTree(self):
-        if self.left:
-            self.left.printTree()
-        print(self.data)
-        if self.right:
-            self.right.printTree()
+	print([f'rootmin = {rootmin}',f'leftmin = {leftmin}', f'rightmin = {rightmin}'])
+	if rootmin < leftmin and rootmin < rightmin:
+		return tree["root"]
+	if leftmin < rootmin and leftmin < rightmin:
+		return leftNode
+	if rightmin < rootmin and rightmin < leftmin:
+		return rightNode
 
 
 if __name__ == "__main__":
-    BSTree = Node()
-    for i in range(10):
-        BSTree.insert(i)
-
-    BSTree.printTree()
+	tree = {
+		"nodes": [
+			{"id": "10", "left": "5", "right": "15", "value": 10},
+			{"id": "15", "left": "13", "right": "22", "value": 15},
+			{"id": "22", "left": None, "right": None, "value": 22},
+			{"id": "13", "left": None, "right": "14", "value": 13},
+			{"id": "14", "left": None, "right": None, "value": 14},
+			{"id": "5", "left": "2", "right": "5-2", "value": 5},
+			{"id": "5-2", "left": None, "right": None, "value": 5},
+			{"id": "2", "left": "1", "right": None, "value": 2},
+			{"id": "1", "left": None, "right": None, "value": 1}
+		],
+		"root": "10"
+	}
+	print(FindClosestNeighbourinBST(tree, 12))
