b0661cf8f558e21f7f3214a5187f98074883dd33
Sathish Kumar <neosathishgct@gmail.com>
2020-12-29 21:23:19 +0530

    FindClosestNeighbourinBST O(log n to the base 2w) time and O(1)

--- algoexpert/FindClosestNeighbourinBST.py
+++ algoexpert/FindClosestNeighbourinBST.py
@@ -1,26 +1,14 @@
-def findClosestNeighbourInBst(tree, target):
-	leftdiff = rightdiff = closestleft = closestright = None
-	
+def findClosestNeighbourInBst(tree, target, closest=None):
+	if tree is None:
+		return closest
+	if closest is None or abs(target - tree.value) < abs(target - closest):
+		closest = tree.value
 	if target < tree.value:
-		if tree.left:
-			closestleft = findClosestNeighbourInBst(tree.left, target)
-			leftdiff = abs(target - closestleft)
-		closestroot = tree.value
-		rootdiff = abs(target - closestroot)
-
-		return closestleft if closestleft is not None and leftdiff < rootdiff else closestroot
-
+		return findClosestNeighbourInBst(tree.left, target, closest)
 	if target > tree.value:
-		if tree.right:
-			closestright = findClosestNeighbourInBst(tree.right, target)
-			rightdiff = abs(target - closestright)
-		closestroot = tree.value
-		rootdiff = abs(target - closestroot)
+		return findClosestNeighbourInBst(tree.right, target, closest)
 
-		return closestright if closestright is not None and rightdiff < rootdiff else closestroot
-	
-	return tree.value
-
+	return closest
 	
 class Node:
 	def __init__(self, value=None):
@@ -43,10 +31,21 @@ class Node:
 		else:
 			self.value = value
 
+	def printTree(self):
+		if self.left:
+			self.left.printTree()
+		print(self.value)
+		if self.right:
+			self.right.printTree()
+
 
 if __name__ == "__main__":
 	BSTree = Node()
-	for i in [10, 15, 22, 13, 14, 5, 5, 2, 1]:
+	#for i in [100, 502, 55000, 1001, 4500, 204, 205, 207, 208, 206, 203, 5, 15, 22, 57, 60, 5, 2, 3, 1, 2, 3, 4, 5, -51, -403]:
+	for i in [10, 15, 22, 13, 14, 5, 5, 2 ,1]:
 		BSTree.insert(i)
 
+	#BSTree.printTree()
+
+	#print(findClosestNeighbourInBst(BSTree, 208))
 	print(findClosestNeighbourInBst(BSTree, 12))
