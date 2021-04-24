ac3b9d51b9908f5d2229b67507c4fba499c045a8
Sathish Kumar <neosathishgct@gmail.com>
2021-04-24 17:54:21 +0530

    Merge overlapping intervals in the given list - O(nlog(n)) time and O(n) space

--- algoexpert/MergeContinuousIntervals.py
+++ algoexpert/MergeContinuousIntervals.py
@@ -1,30 +1,28 @@
 def mergeContinuousIntervals(intervals):
-    #[[1,2], [3, 5], [4, 7], [6, 8], [9, 10]]
-    #
-    #ANSWER: [[1,2],[3, 8], [9, 10]]
+	intervals.sort() #O(nlog(n)) time
+	result = list() #O(n) space
+	i = 0
+	while i < len(intervals):
+		currentInterval = intervals[i]
 
-    result = list()
-    i = 0
-    while i < len(intervals):
-        resultInterval = intervals[i]
+		while i < len(intervals) - 1:
+			nextInterval = intervals[i + 1]
 
-        while i < len(intervals) - 1:
-            nextInterval = intervals[i + 1]
-            if resultInterval[1] >= nextInterval[0]:
-                resultInterval = [resultInterval[0], nextInterval[1]]
-#                print(f'inside while: {resultInterval}')
-#                print(f'inside while: {i}')
-                i += 1
-            else:
-#                print(f'inside else: {i}')
-                break
+			if currentInterval[1] >= nextInterval[0]:
+				currentInterval = [min(currentInterval[0], nextInterval[0]), max(currentInterval[1], nextInterval[1])]
+				i += 1
+			else:
+				break
 
-#        print(resultInterval)
-        result.append(resultInterval)
-        i += 1
+		result.append(currentInterval)
+		i += 1
 
-    return result
+	return result
 
 
 if __name__ == "__main__":
-    print(mergeContinuousIntervals([[1,2], [3, 5], [4, 7], [6, 8], [9, 10]]))
+	print(mergeContinuousIntervals([[1,2], [3, 5], [4, 7], [6, 8], [9, 10]]))
+	print(mergeContinuousIntervals([[100,105], [1, 104]]))
+	print(mergeContinuousIntervals([[89, 90], [-10, 20], [-50, 0], [70, 90], [90, 91], [90, 95]]))
+	print(mergeContinuousIntervals([[20, 21], [22, 23], [0, 1], [3, 4], [23, 24], [25, 27], [5, 6], [7, 19]]))
+	print(mergeContinuousIntervals([[2, 3], [4, 5], [6, 7], [8, 9], [1, 10]]))
