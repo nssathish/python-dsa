b33560b9f9297e1fa673184f8bf4dceb8293b125
Sathish Kumar Subramanian <nsubramanian.sathish@gmail.com>
2021-11-27 07:31:28 +0530

    Code with Mosh - DSA - stack - Balanced Expresession

--- codewithmosh-dsa/stack/BalancedExpression.py
+++ codewithmosh-dsa/stack/BalancedExpression.py
@@ -2,7 +2,7 @@ from StackOps import Stack
 
 
 def isBalanced(expression: str) -> bool:
-    new_stack = Stack(len(expression))
+    new_stack = Stack(size = len(expression))
     
     for character in expression:
         if isOpenBracket(character):
@@ -12,13 +12,14 @@ def isBalanced(expression: str) -> bool:
     
     return new_stack.isEmpty()
 
-def bracketsMatch(left: str, right: str) -> bool:
-    return (character == ")" and new_stack.peek() == "(") or \
-            (character == ']' and new_stack.peek() == "[') or \
-                (character == '}' and new_stack.peek() == '{') or \
-                    (character == '>' and new_stack.peek() == '<')
- 
- def indexOf(character)
+def bracketsMatch(new_stack: Stack, character: str) -> bool:
+    return indexOf(new_stack.peek(), open_brackets()) == indexOf(character, closed_brackets())
+
+def indexOf(character: str, brackets: list) -> int:
+    try:
+        return brackets.index(character)
+    except ValueError as ve:
+        return -1
 
 def isOpenBracket(character) -> bool:
     return character in open_brackets()
@@ -26,6 +27,8 @@ def isOpenBracket(character) -> bool:
 def open_brackets():
     return ['(', '[', '{', '<']
 
+def closed_brackets():
+    return [')', ']', '}', '>']
 
 if __name__ == '__main__':
     expression = '[1234(23+34))'
