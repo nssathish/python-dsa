f1dc41c7d22f34c1d786e57c75e63d88c44bfb5c
Sathish Kumar Subramanian <nsubramanian.sathish@gmail.com>
2021-11-28 22:49:57 +0530

    Code with Mosh - DSA - Queue - Queue operations

---
+++ codewithmosh-dsa/queue/QueueOps.py
@@ -0,0 +1,50 @@
+from typing import Sized
+
+
+class Queue:
+    def __init__(self, size=0) -> None:
+        self.size = size
+        self.queue = []
+    
+    def enqueue(self, value: int) -> None:
+        try:
+            if self.isFull():
+                raise ValueError("Queue limit reached")
+            
+            self.queue.append(value)
+        except ValueError as ve:
+            print(ve)
+    
+    def dequeue(self) -> int:
+        try:
+            if self.isEmpty():
+                raise ValueError("Queue empty")
+            
+            first = self.queue[0]
+            del self.queue[0]
+            return first
+        except ValueError as ve:
+            print(ve)
+    
+    def peek(self) -> int:
+        try:
+            if self.isEmpty():
+                raise ValueError("Queue empty")
+
+            return self.queue[0]
+        except ValueError as ve:
+            print(ve)
+    
+    def isFull(self) -> bool:
+        return len(self.queue) == self.size
+    
+    def isEmpty(self) -> bool:
+        return len(self.queue) == 0
+    
+
+if __name__ == '__main__':
+    new_queue = Queue(5)
+    new_queue.enqueue(5); new_queue.enqueue(2); new_queue.enqueue(10); new_queue.enqueue(1); new_queue.enqueue(6)
+    print(new_queue.peek())
+    print(new_queue.dequeue())
+    print(new_queue.peek())
